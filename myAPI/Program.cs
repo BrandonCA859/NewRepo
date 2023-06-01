3889ceabcdac563cfe4bf7f58914d44c0a58ea49
Brandon Carrillo Álvarez <128259306+BrandonCA859@users.noreply.github.com>
2023-05-31 21:44:07 -0600

    XKZMZNXZ

--- myAPI/Program.cs
+++ myAPI/Program.cs
@@ -1,8 +1,23 @@
+using DataAccess;
+using Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDataAccess();
+builder.Services.AddServices();
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+//builder.Services.AddControllers();
+
+//builder.Services.AddControllers();
+
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+);
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
