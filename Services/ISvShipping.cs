fa800a1936b382a455ec6f3c297159837b9be3ee
Norvez <normanalvarado123@gmail.com>
2023-06-02 16:29:36 -0600

    SvShipping,ISvShipping, y lo que se necesita... no he hecho el controlador. este si

---
+++ Services/ISvShipping.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class ISvShipping
+    {
+        public Shipping Add(Shipping shipping);
+
+     //   public List<Shipping> ListShipping();
+
+       // public void Update(Shipping shipping);
+       // public void Delete(int id);
+    }
+}
