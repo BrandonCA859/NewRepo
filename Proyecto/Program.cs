513bf4b188fcd94f2904e8e28f3477ae0315edbe
Geraldsamurai3 <132635163+Geraldsamurai3@users.noreply.github.com>
2023-06-01 23:40:21 -0600

    Codigo actualizado (#14)
    * Add new User controller and Json method failed
    
    * add controller

--- Proyecto/Program.cs
+++ Proyecto/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DataAccess.Entidades;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Services;
+using DataAccess;
+
+IHost app = Host.CreateDefaultBuilder() // El Host es el contexto donde corre nuestra aplicación, y tiene opciones tipo Compilador y de Configurar Servicios
+.ConfigureServices((context, services) => // El Configure Services puede acceder a los otros proyectos externos
+{
+    services.AddDataAccess(); // Hacemos que el Program sepa de la existencia del proyecto DataAccess y sus servicios configurados, en este caso el MyDbContext
+    services.AddServices(); // Hacemos que el Program sepa de la existencia del proyecto Services y sus servicios configurados, en este caso SvUser por medio de IUser
+
+}).Build();//El Build inicia el Host y compila los proyectos externos para que sus servicios configurados sean entendidos por la aplicación principal
+
+var svUser = app.Services.GetRequiredService<ISvUser>();
+
+
+
+
+
+List<User> usersList = svUser.ListUsers();
+
+
+
+
