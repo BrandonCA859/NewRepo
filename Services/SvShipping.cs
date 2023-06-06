adab127997dd228ec14360cf0582ac2b297f5281
Norvez <normanalvarado123@gmail.com>
2023-06-05 23:10:09 -0600

    ok. aqui estan los cambios necesarios para todo lo que es shipping

---
+++ Services/SvShipping.cs
@@ -0,0 +1,33 @@
+﻿using DataAccess;
+using DataAccess.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class SvShipping : ISvShipping
+    {
+        private readonly MyDbContext _myDbContext;
+
+        public SvShipping(MyDbContext myDbContext)
+        {
+            _myDbContext = myDbContext;
+        }
+
+        public Shipping Add(Shipping shipping)
+        {
+            _myDbContext.Shippings.Add(shipping);
+            _myDbContext.SaveChanges();
+
+            return shipping;
+        }
+
+        public List<Shipping> ListShippings()
+        {
+            return _myDbContext.Shippings.ToList();
+        }
+    }
+}
--- myAPI/Controllers/ShippingController.cs
+++ myAPI/Controllers/ShippingController.cs
@@ -1,8 +1,47 @@
-﻿using System;
+﻿using DataAccess.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+using System.Collections.Generic;
 
-public class Class1
+namespace myAPI.Controllers
 {
-	public Class1()
-	{
-	}
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShippingController : ControllerBase
+    {
+        private readonly ISvShipping _svShipping;
+
+        public ShippingController(ISvShipping svShipping)
+        {
+            _svShipping = svShipping;
+        }
+
+        // GET: api/<ShippingController>
+        [HttpGet]
+        public IEnumerable<Shipping> Get()
+        {
+            return _svShipping.ListShippings();
+        }
+
+        // GET api/<ShippingController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ShippingController>
+        [HttpPost]
+        public void Post([FromBody] Shipping value)
+        {
+            _svShipping.Add(value);
+        }
+
+        // POST api/<ShippingController>
+        [HttpPost("{id}")]
+        public void Add(int id, [FromBody] Shipping value)
+        {
+            _svShipping.Add(value);
+        }
+    }
 }
